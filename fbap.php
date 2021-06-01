62eb68940d4b51dd91175576e5bdd3509b6a6b87
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2021-06-01 16:59:34 +0300

    fresh wordpress plugin boilerplate

--- fbap.php
+++ fbap.php
@@ -8,20 +8,20 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              https://github.com/XTishka/wp_plugin_fbap
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           FBAP
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin Name:       Facebook affiliate links publisher
+ * Plugin URI:        https://github.com/XTishka/wp_plugin_fbap
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Takhir Berdyiev
+ * Author URI:        https://xtf.com.ua/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       fbap
  * Domain Path:       /languages
  */
 
